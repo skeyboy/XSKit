016572091ae50d9ad69a39a6cc9efb445a730972
le <le@ledeMacBook-Pro.local>
2018-04-16 17:27:48 +0800

    提供简单的safe array

--- XSKit/XSKit/Foundation/Array+XSAdd.swift
+++ XSKit/XSKit/Foundation/Array+XSAdd.swift
@@ -7,8 +7,44 @@
 //
 
 import Foundation
+let lock = DispatchSemaphore(value: 1)
+
 extension Array{
-    func test(){
+    func safe( safe:()->Void){
+        let _: DispatchTimeoutResult = lock.wait(timeout: .distantFuture)
+            safe()
+        lock.signal()
+       
+    }
+}
+extension Array{
+    var first: Element? {
+        var tmp:Element? = nil
+        safe {
+            tmp = self.first
+        }
+        return tmp
+    }
+    var last: Element? {
+        var tmp:Element? = nil
+        safe {
+            tmp = self.last
+        }
+        return tmp
+    }
+    
+}
+extension Array{
+    func append<Element>(element:Element) -> Void {
+        safe {
+            self.append(element: element)
+        }
+    }
+    func  insert<Element>(newElement: Element, at: Int){
+        safe {
+            self.insert(newElement: newElement, at: at)
+        }
         
     }
+
 }
