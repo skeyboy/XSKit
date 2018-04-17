8a0769defe771c32c78cd528811fb5d7daa18f4f
le <le@ledeMacBook-Pro.local>
2018-04-17 10:44:27 +0800

    Device 一些硬件特性，有待完善

--- XSKit/XSKit/UIKit/UIDevice+XSAdd.swift
+++ XSKit/XSKit/UIKit/UIDevice+XSAdd.swift
@@ -25,6 +25,47 @@ return false
         return UIApplication.shared.canOpenURL(URL(string: "tel://")!)
     }
 }
+extension Device{
+    var cpuCount:Int{
+        return ProcessInfo.processInfo.activeProcessorCount
+    }
+    var memoryTotal:UInt64{
+      return  ProcessInfo.processInfo.physicalMemory
+    }
+    var memoryUsed:Int64 {
+        let host_port:mach_port_t =  mach_host_self()
+        
+        let host_size:UnsafeMutablePointer<mach_msg_type_number_t> = UnsafeMutablePointer<mach_msg_type_number_t>.allocate(capacity: MemoryLayout.size(ofValue: vm_statistics_data_t())/MemoryLayout.size(ofValue: integer_t()))
+        
+        let page_size:UnsafeMutablePointer<vm_size_t> = UnsafeMutablePointer<vm_size_t>.allocate(capacity: 0)
+        
+       var kern = host_page_size(host_port, page_size)
+        if kern != KERN_SUCCESS {
+            return -1
+        }
+        let vm_stat:(Any)? = nil
+        kern = host_statistics(host_port, HOST_VM_INFO, vm_stat as! host_info_t,host_size )
+        if kern != KERN_SUCCESS {
+            return -1
+        }
+        
+        return Int64((vm_stat as! vm_statistics_data_t).free_count) * Int64(page_size.pointee)
+        
+    }
+    var diskSpaceFree:Int64{
+        let attrs = try! FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())
+        
+        return attrs[FileAttributeKey.systemFreeSize] as! Int64
+    }
+    var diskSpace:Int64 {
+        let attrs = try! FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())
+        return attrs[FileAttributeKey.systemSize] as! Int64
+    }
+    var diskSpaceUsed:Int64 {
+        
+        return min(diskSpace - diskSpaceFree, diskSpace)
+    }
+}
 extension Device {
     public var isJailbroken:Bool{
         if  isSimulator() {
